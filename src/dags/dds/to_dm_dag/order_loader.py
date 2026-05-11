934b556de2447b8b0891a138aa216c476e65f18d
@kir.presnyakov <kir.prsv@inbox.ru>
2026-05-11 13:17:34 +0300

    Update order_loader.py

--- src/dags/dds/to_dm_dag/order_loader.py
+++ src/dags/dds/to_dm_dag/order_loader.py
@@ -87,19 +87,18 @@ class OrdersLoader:
         self.log = log
 
     def load_data(self):
-        # открываю транзакцию.
-        # Транзакция будет закоммичена, если код в блоке with пройдет успешно (т.е. без ошибок).
+        # Открываем транзакцию: если код в блоке with пройдет успешно => будет закоммичена
         # Если возникнет ошибка, произойдет откат изменений (rollback транзакции).
         with self.pg_dest.connection() as conn:
 
-            # Прочитываю состояние загрузки
-            # Если настройки еще нет, создаю ее.
+            # Получаем состояние загрузки
+            # Если настройки еще нет, создаем ее.
             wf_setting = self.settings_repository.get_setting(conn, self.WF_KEY)
             self.log.info(f'wf_setting = {wf_setting}')
             if not wf_setting:
                 wf_setting = EtlSetting(id=0, workflow_key=self.WF_KEY, workflow_settings={self.LAST_LOADED_ID_KEY: -1})
 
-            # Вычитываю очередную пачку объектов.
+            # Вычитываем очередную пачку объектов.
             last_loaded = wf_setting.workflow_settings[self.LAST_LOADED_ID_KEY]
             self.log.info(f'last_loaded = {last_loaded}')
             self.log.info(f'BATCH_LIMIT = {self.BATCH_LIMIT}')
@@ -109,7 +108,7 @@ class OrdersLoader:
                 self.log.info("Quitting.")
                 return
 
-            # Сохраняю объекты в базу dwh.
+            # Сохраняем объекты в базу dwh.
             for object in load_queue:
                 try:
                     self.dds.insert_object(conn, object)
@@ -118,9 +117,7 @@ class OrdersLoader:
                     print('Error =', err) 
                     raise 
 
-            # Сохраняю прогресс.
-            # Пользуюсь тем же connection, поэтому настройка сохранится вместе с объектами,
-            # либо откатятся все изменения целиком.
+            # Сохраняем прогресс.
             wf_setting.workflow_settings[self.LAST_LOADED_ID_KEY] = max([t.id for t in load_queue])
             wf_setting_json = json2str(wf_setting.workflow_settings)  # Преобразую к строке, чтобы положить в БД.
             self.settings_repository.save_setting(conn, wf_setting.workflow_key, wf_setting_json)
